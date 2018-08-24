f7d3daafe7a7772d1eee390561ec76857d6fd8ff
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2018-08-24 14:49:07 -0400

    remove index when dir undefined

--- index.js
+++ index.js
@@ -7,19 +7,25 @@ var DatasaurBase = require('datasaur-base');
 var DatasaurSimpleSort = DatasaurBase.extend('DatasaurSimpleSort', {
 
     sort: function(x, options) {
-        var source = this.next.next;
-        var index = Array(source.getRowCount());
-        for (var i = index.length; i--; ) index[i] = i;
+        var index;
+        var dir = options && options.dir || '';
 
-        var comparator = options && options.comparator ? options.comparator.bind(source, x) : function(y1, y2) {
-            var v1 = source.getValue(x, y1), v2 = source.getValue(x, y2);
-            return v1 < v2 ? -1 : v1 > v2 ? 1 : 0 ;
-        };
+        if (dir) {
+            var source = this.next.next;
 
-        index.sort(comparator);
+            index = Array(source.getRowCount());
+            for (var i = index.length; i--;) index[i] = i;
 
-        if ((options && options.dir || '').toLowerCase() === 'desc') {
-            index.reverse();
+            var comparator = options && options.comparator ? options.comparator.bind(source, x) : function (y1, y2) {
+                var v1 = source.getValue(x, y1), v2 = source.getValue(x, y2);
+                return v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+            };
+
+            index.sort(comparator);
+
+            if (dir.toLowerCase() === 'desc') {
+                index.reverse();
+            }
         }
 
         return this.setIndex(index);
